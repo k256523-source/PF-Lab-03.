14f02ddbc4c1d92c646eceab1278a1f980839bba
k256523-source <k256523@nu.edu.pk>
2026-02-08 17:14:02 +0500

    Create intro.c
    Includes basic structure of C language.

---
+++ intro.c
@@ -0,0 +1,3 @@
+A C program follows a rigid, top-down structure that begins with preprocessor directives (like #include <stdio.h>) to load essential libraries, followed by the int main() function,  
+which serves as the mandatory entry point for execution. Inside this function, all instructions are wrapped in curly braces { } and must end with a semicolon ; to signal the completion of a statement. 
+The process typically concludes with a return 0; statement, which tells the operating system that the program ran successfully.
